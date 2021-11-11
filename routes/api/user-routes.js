aef34e7f33e10a0ec8ba604e628974c4e9e5e580
Tari <bota.maral@gmail.com>
2021-11-11 10:14:05 -0500

    clean up

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// Get / api/ users
+// Get all users
 router.get('/', (req, res) => {
     User.findAll({
-      attributes: { exclude: ['password']}
-    }).then(dbUserData => res.json(dbUserData))
+      attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
